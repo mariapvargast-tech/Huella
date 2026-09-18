e858b763c652b01ceb3d2f7cb82122a42b94de80
Jose Miguel Velez Ramirez <jose.velez_r@hotmail.com>
2026-09-18 17:33:21 -0500

    Se realiza proyecto del entregable con el webview

---
+++ app/src/main/java/com/jmvr/rescatandohuellas/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.jmvr.rescatandohuellas
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.jmvr.rescatandohuellas.ui.theme.RescatandoHuellasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RescatandoHuellasTheme {
+
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Navegador(
+                        url = "https://www.lowshopping.com",
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+@Composable
+fun Navegador(url: String, modifier: Modifier = Modifier) {
+    AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                settings.javaScriptEnabled = true
+                webViewClient = WebViewClient()
+                loadUrl(url)
+            }
+        },
+        modifier = modifier
+    )
+}

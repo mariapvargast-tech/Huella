c1861e6f8b9b2e36ea660328885e86c06cec882d
Jose Miguel Velez Ramirez <jose.velez_r@hotmail.com>
2026-09-18 17:35:16 -0500

    Se cambia URL a una  general

--- app/src/main/java/com/jmvr/rescatandohuellas/MainActivity.kt
+++ app/src/main/java/com/jmvr/rescatandohuellas/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : ComponentActivity() {
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Navegador(
-                        url = "https://www.lowshopping.com",
+                        url = "https://www.google.com",
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(innerPadding)
